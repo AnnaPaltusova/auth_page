6b6865138c1d7f30a6bd047d4aac423c281f6571
AnnaPaltusova <anna.l.andronova@gmail.com>
2022-03-11 00:34:28 +0500

    Добавлен лого и фон

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,10 @@
 import 'package:flutter/material.dart';
+import 'auth_page_demo.dart';
+import 'navig_drawer_screen.dart';
+import 'appBar_screen.dart';
+import 'navi_demo.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(const NavigationDemo());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}): super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    const borderStyle= OutlineInputBorder(
-      borderRadius: BorderRadius.all(Radius.circular(36)),
-      borderSide: BorderSide(
-        color: const Color(0xFFbbbbbb), width: 2));
-    const LinkTextStyle = TextStyle(
-      fontSize: 18,
-      fontWeight: FontWeight.bold,
-      color: Color(0xFF0079D0)
-      );
-
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/bg1.jpg"),
-              fit: BoxFit.cover,
-            ),
-          ),
-          width: double.infinity,
-          height: double.infinity,
-           padding: EdgeInsets.symmetric(horizontal: 50,),
-           child: SingleChildScrollView(
-              child: Column(children: [
-                SizedBox(height: 150,),
-                const SizedBox(width: 110, height: 84, child: const Image(image: AssetImage('assets/dart-logo.png')),),
-                SizedBox(height: 20,),
-                Text('Введите логин в виде 10 цифр номера телефона',
-                style: TextStyle(fontSize: 18, color: Color.fromRGBO(0, 0, 0, 6)),),
-                SizedBox(height: 20,),
-                TextField(
-                  keyboardType: TextInputType.phone,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Color(0xFFeceff1),
-                    enabledBorder: borderStyle,
-                    focusedBorder: borderStyle,
-                    labelText: 'Телефон',
-                  ),
-                ),
-                SizedBox(height: 20,),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Color(0xFFeceff1),
-                    enabledBorder: borderStyle,
-                    focusedBorder: borderStyle,
-                    labelText: 'Пароль',
-                  ),
-                ),
-                SizedBox(height: 28,),
-                SizedBox(width: 154, height: 42, child:
-                ElevatedButton(onPressed: () {},
-                    child: Text('Войти'),
-                style: ElevatedButton.styleFrom(
-                  primary: Color(0xFF0079D0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(22.0),
-                    ),
-                ))),
-                SizedBox(height: 32,),
-                InkWell(child: Text('Регистрация', style: LinkTextStyle,),
-                  onTap: () {}),
-                SizedBox(height: 20,),
-                InkWell(child: Text('Забыли пароль?', style: LinkTextStyle,), onTap: () {}),
-              ],),
-            ),
-          ),
-        ),
-      );
-  }
-  }
